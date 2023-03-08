5c8bd46560718704345c20052ddc9c6f263ce18d
abelman0 <abelman0011@gmail.com>
2023-03-08 15:21:38 +0300

    first commit

---
+++ git/git.js
@@ -0,0 +1,23 @@
+/*
+// or create a new repository on the command line 
+ frist use
+  1 git init
+  2 git add .
+
+    if you want to check the status
+ *# git status     // is a command that provides a more detailed overview of the current state of your repository, 
+                   // including information about changes that have been made to tracked files, changes that have been staged, and untracked files.
+                   //also provides guidance on what you can do next, such as which files need to be staged and committed.
+    if you want to check the ls in the git
+  # git ls-files    // is a command that simply lists all the files that are currently being tracked by Git in your repository
+ 
+  3 git commit -m "first commit"
+
+  # git remote -v                                  // List the current remotes for your repository using this 
+ *# git remote add <remote_name> <remote_url>     // Add a new remote repository using the command: <remote_name> can be any name you choose for the remote,
+  # git remote remove origin                     // Remove the origin remote using this command: if you name <remote_name> =  orgin
+  # git remote set-url origin <new_remote_url>  //  if you want to update the existing origin remote URL
+  
+ 
+ 
+ */ 
